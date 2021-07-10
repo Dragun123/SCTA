285e50f01249b7f914b1804e1e36f792783fabcc
Dragun <poyosh2@aol.com>
2021-07-09 21:37:02 -0400

    FinalUpdateBeforeShowMatch

---
+++ projectiles/ARM_SNIPE/ARM_SNIPE_script.lua
@@ -0,0 +1,12 @@
+#ARM Zipper Laser
+#ARM_FAST
+#
+#Script created by Raevn
+
+local TABlueLaserProjectile = import('/mods/SCTA-master/lua/TAprojectiles.lua').TABlueLaserProjectile
+
+ARM_SNIPE = Class(TABlueLaserProjectile) 
+{}
+
+TypeClass = ARM_SNIPE
+
