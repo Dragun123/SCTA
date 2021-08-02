e556ad6501e9b32e3414bcc33cbbef41a248c755
Dragun <poyosh2@aol.com>
2021-08-01 20:57:42 -0400

    SCTA LightingTank and Snipers

--- units/ARMLATNK/ARMLATNK_Script.lua
+++ units/ARMLATNK/ARMLATNK_Script.lua
@@ -3,10 +3,10 @@
 #
 #Script created by Raevn
 
-local TACounter = import('/mods/SCTA-master/lua/TAMotion.lua').TACounter
+local TAunit = import('/mods/SCTA-master/lua/TAunit.lua').TAunit
 local TAweapon = import('/mods/SCTA-master/lua/TAweapon.lua').TAweapon
 
-ARMLATNK = Class(TACounter) {
+ARMLATNK = Class(TAunit) {
 	Weapons = {
 		WEAPON = Class(TAweapon) {},
 	},
