4f02919d750d8b13729bab83579e71965969b183
Dragun <poyosh2@aol.com>
2021-08-19 17:53:45 -0400

    MinorReworksOfPlatforms

--- units/CORHP/CORHP_script.lua
+++ units/CORHP/CORHP_script.lua
@@ -3,56 +3,28 @@
 #
 #Script created by Raevn
 
-local TAFactory = import('/mods/SCTA-master/lua/TAFactory.lua').TAFactory
+local TASeaPlat = import('/mods/SCTA-master/lua/TAFactory.lua').TASeaPlat
 
-CORHP = Class(TAFactory) {
-	OnCreate = function(self)
-		self.Spinners = {
-			beam1 = CreateRotator(self, 'beam1', 'y', nil, 0, 0, 0),
-			beam2 = CreateRotator(self, 'beam2', 'y', nil, 0, 0, 0),
-			beam3 = CreateRotator(self, 'beam3', 'y', nil, 0, 0, 0),
-			beam4 = CreateRotator(self, 'beam4', 'y', nil, 0, 0, 0),
-		}
-			for k, v in self.Spinners do
-				self.Trash:Add(v)
-			end
-		TAFactory.OnCreate(self)
+CORHP = Class(TASeaPlat) {
+
+    WaterFall = function(self)
+		if self.Water then
+			self.Chassis:SetSpeed(10)
+			self.Chassis:SetGoal(0,-10,0)
+			self:SetCollisionShape( 'Box', self.bp.CollisionOffsetX or -5,(self.bp.CollisionOffsetY + (self.bp.SizeY*-0.5)) or 0,self.bp.CollisionOffsetZ or -5, self.bp.SizeX * self.scale, self.bp.SizeY * self.scale, self.bp.SizeZ * self.scale )
+			self:EnableIntel('RadarStealth')
+		end
 	end,
 
 
 	Open = function(self)
-		TAFactory.Open(self)
-		self.Spinners.beam1:SetGoal(90)
-		self.Spinners.beam1:SetSpeed(175)
-		
-		--TURN door2 to z-axis <90.00> SPEED <175.13>;
-		self.Spinners.beam2:SetGoal(90)
-		self.Spinners.beam2:SetSpeed(175)
-		
-		self.Spinners.beam3:SetGoal(-90)
-		self.Spinners.beam3:SetSpeed(175)
-		
-		--TURN door2 to z-axis <90.00> SPEED <175.13>;
-		self.Spinners.beam4:SetGoal(-90)
-		self.Spinners.beam4:SetSpeed(175)
-	end,
-
-	Close = function(self)
-		TAFactory.Close(self)
-
-		self.Spinners.beam1:SetGoal(0)
-		self.Spinners.beam1:SetSpeed(175)
-		
-		--TURN door2 to z-axis <90.00> SPEED <175.13>;
-		self.Spinners.beam2:SetGoal(0)
-		self.Spinners.beam2:SetSpeed(175)
-
-		self.Spinners.beam3:SetGoal(0)
-		self.Spinners.beam3:SetSpeed(175)
-
---TURN door2 to z-axis <90.00> SPEED <175.13>;
-		self.Spinners.beam4:SetGoal(0)
-		self.Spinners.beam4:SetSpeed(175)
+		if self.Water then
+			self.Chassis:SetSpeed(10)
+			self.Chassis:SetGoal(0,0,0)
+			self:SetCollisionShape( 'Box', self.bp.CollisionOffsetX or 0,(self.bp.CollisionOffsetY + (self.bp.SizeY*0.5)) or 0,self.bp.CollisionOffsetZ or 0, self.bp.SizeX * self.scale, self.bp.SizeY * self.scale, self.bp.SizeZ * self.scale )
+			self:DisableIntel('RadarStealth')
+		end
+        TASeaPlat.Open(self)
 	end,
 }
 
