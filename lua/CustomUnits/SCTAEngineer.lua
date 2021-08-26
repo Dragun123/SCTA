1e2ab196045401aa22741f09c6d4193def08387f
Dragun <poyosh2@aol.com>
2021-08-26 15:47:27 -0400

    CustomUnit Testing

---
+++ lua/CustomUnits/SCTAEngineer.lua
@@ -0,0 +1,18 @@
+UnitList = {
+    T1BuildEngineer = {
+            Arm = {'armck', 100},
+            Core = {'corcv', 100},
+          },
+    T2BuildEngineer = {
+            Arm = {'armack', 100},
+            Core = {'coracv', 100},
+          },
+    T3BuildEngineer = {
+            Arm = {'armch', 100},
+            Core = {'corch', 100},
+          },
+    T3LandSubCommander = {
+            Arm = {'armdecom', 100},
+            Core = {'cordecom', 100},
+          },
+	}
---
+++ lua/CustomUnits/SCTALand.lua
@@ -0,0 +1,78 @@
+UnitList = {
+    T1LandAA = {
+        Arm = {'armjeth', 100},
+        Core = {'cormist', 100},
+	},
+    T2LandAA = {
+        Arm = {'armamph', 100},
+        Core = {'corsent', 100},
+    },
+    T3LandAA = {
+        Arm = {'armah', 100},
+        Core = {'corah', 100},
+    },
+    T1LandDFTank = {
+        Arm = {'armwar', 100},
+        Core = {'corraid', 100},
+	},
+    T2LandDFTank = {
+        Arm = {'armfido', 100},
+        Core = {'correap', 100},
+    },
+    T2AttackTank = {
+        Arm = {'armbull', 100},
+        Core = {'corcan', 100},
+    },
+    T3LandBot = {
+        Arm = {'armlatnk', 100},
+        Core = {'corpyro', 100},
+    },
+    T1LandDFBot = {
+        Arm = {'armpw', 100},
+        Core = {'corgator', 100},
+    },
+    T2LandAmphibious = {
+        Arm = {'armcroc', 100},
+        Core = {'corseal', 100},
+    },
+    T1LandArtillery = {
+        Arm = {'armham', 100},
+        Core = {'corlevlr', 100},
+	},
+    T2LandArtillery = {
+        Arm = {'armrock', 100},
+        Core = {'corvroc', 100},
+    },
+    T3LandArtillery = {
+        Arm = {'armmart', 100},
+        Core = {'cormart', 100},
+	},
+    T3SniperBots = {
+        Arm = {'armsnipe', 100},
+        Core = {'cormort', 100},
+    },
+    T3ArmoredAssault = {
+        Arm = {'armzues', 100},
+        Core = {'corgol', 100},
+	},
+    T3MobileMissile = {
+        Arm = {'armmh', 100},
+        Core = {'cormh', 100},
+    },
+    T2MobileShields = {
+        Arm = {'armjam', 100},
+        Core = {'coreter', 100},
+    },
+    T3MobileShields = {
+        Arm = {'armseer', 100},
+        Core = {'corvrad', 100},
+	},
+    T2MobileBombs = {
+        Arm = {'armvader', 100},
+        Core = {'corroach', 100},
+    },
+    T1LandScout = {
+        Arm = {'armflea', 100},
+        Core = {'corfav', 100},
+    },
+}
