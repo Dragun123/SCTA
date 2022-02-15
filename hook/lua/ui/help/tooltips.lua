d24d57dbdf59a06ceec9902e2312c03a8d262d2d
Dragun123 <66501472+Dragun123@users.noreply.github.com>
2022-02-15 15:10:10 -0500

    Tooltips

--- hook/lua/ui/help/tooltips.lua
+++ hook/lua/ui/help/tooltips.lua
@@ -5,7 +5,7 @@
     } 
     Tooltips.lob_core = { 
         title = "CORE", 
-	description = "A rulers of the universe from a prior era who sought immortality for mankind via transferring the mind into the machine"
+	    description = "A rulers of the universe from a prior era who sought immortality for mankind via transferring the mind into the machine"
     } 
     Tooltips.mfd_idle_fieldengineer = {
         title = "Idle Field Engineers",
@@ -14,4 +14,19 @@
     Tooltips.mfd_idle_terrainengineer = {
         title = "Idle Speciality Terrain Engineers",
         description = "Idle Speciality Terrain Engineers",
+    }
+
+    Tooltips.DGun = {
+        title = "Disintegrator Gun",
+        description = "Requires 500 Energy to Fire. Right-click turn on Auto",
+    }
+
+    Tooltips.Radar_Targeting = {
+        title = "Radar Targeting",
+        description = "Enables shooting of blips",
+    }
+
+    Tooltips.toggle_Cloakfieldta = {
+        title = "Cloak Field",
+        description = "Turns the selected unit cloak field on/off",
     }
