19e32986696c1d926529d0792739ea8c1a6a5257
Dragun123 <66501472+Dragun123@users.noreply.github.com>
2022-02-15 13:32:38 -0500

    MassFixingCommas

--- hook/lua/ui/help/tooltips.lua
+++ hook/lua/ui/help/tooltips.lua
@@ -1,26 +1,17 @@
-do
-
     -- TODO: make sure all tooltips are in
-    
-    local TATooltips = {
-    lob_arm = { 
+    Tooltips.lob_arm = { 
         title = "ARM", 
-	description = "A group of rebels from another era who refuse to transfer their mind into the machine"
-    }, 
-    lob_core = { 
+	    description = "A group of rebels from another era who refuse to transfer their mind into the machine"
+    } 
+    Tooltips.lob_core = { 
         title = "CORE", 
-	description = "A rulers of the universe from prior era who sought immortality for mankind via transferring the mind to machine"
-    }, 
-    mfd_idle_fieldengineer = {
+	description = "A rulers of the universe from a prior era who sought immortality for mankind via transferring the mind into the machine"
+    } 
+    Tooltips.mfd_idle_fieldengineer = {
         title = "Idle Field Engineers",
         description = "Idle Field Engineers",
-    },
-    mfd_idle_terrainengineer = {
+    }
+    Tooltips.mfd_idle_terrainengineer = {
         title = "Idle Speciality Terrain Engineers",
         description = "Idle Speciality Terrain Engineers",
-    },
-}
-Tooltips = table.merged( Tooltips, TATooltips )
-
-end
-
+    }
